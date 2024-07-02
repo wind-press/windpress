c833b5a345b9f6b7c105546c4c08047fd5fca163
Joshua Gugun Siagian <suabahasa@gmail.com>
2024-07-02 13:04:59 +0700

    fix mockup

--- assets/admin/components/Workspace/MainPanel.vue
+++ assets/admin/components/Workspace/MainPanel.vue
@@ -9,7 +9,7 @@ const ui = useUIStore();
 
 <template>
     <div class="main-panel w:full flex flex:column flex-grow:1">
-        <div class="tab-head flex flex:row {cursor:pointer;user-select:none;px:18;py:10;bt:1|solid|transparent;br:1|solid|gray-20/.5;bb:1|solid|gray-20/.5;fg:gray-60}>.tab-head__item {bg:white}>.tab-head__item:hover {bg:white;bb:gray-20/.2;bt:1|solid|sky-80;fg:gray-95}>.tab-head__item.active">
+        <div class="tab-head flex flex:row {cursor:pointer;user-select:none;px:18;py:10;bt:1|solid|transparent;br:1|solid|gray-20/.5;bb:1|solid|gray-20/.5;fg:gray-60}>.tab-head__item,>.tab-head__space {bg:white}>.tab-head__item:hover {bg:white;bb:gray-20/.2;bt:1|solid|sky-80;fg:gray-95}>.tab-head__item.active">
             <div :class="{ active: ui.virtualState('main-panel.tab.active', 'main.css').value === 'main.css' }" @click="ui.virtualState('main-panel.tab.active', 'main.css').value = 'main.css'" class="tab-head__item">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 54 33" class="h:1em mr:2 vertical-align:-0.125em">
                     <g clip-path="url(#prefix__clip0)">
@@ -31,7 +31,7 @@ const ui = useUIStore();
                 <font-awesome-icon :icon="['fas', 'gear']" class="mr:2" />
                 Settings
             </div>
-            <div class="tab-head__item flex-grow:1 br:hidden!"></div>
+            <div class="tab-head__space flex-grow:1 br:hidden!"></div>
         </div>
         <div class="tab-body h:full bg:white">
             <div class="content-panel h:full">
