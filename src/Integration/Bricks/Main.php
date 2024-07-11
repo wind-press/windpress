199070a25aa9c1d0995ac29e45abf3655b91d1b9
Joshua Gugun Siagian <suabahasa@gmail.com>
2024-07-11 20:36:23 +0700

    bricks initial integration

--- src/Integration/Bricks/Main.php
+++ src/Integration/Bricks/Main.php
@@ -25,11 +25,12 @@ class Main implements IntegrationInterface
     {
         add_filter('f!windpress/core/cache:compile.providers', fn (array $providers): array => $this->register_provider($providers));
 
-        // if ($this->is_enabled()) {
-        //     add_filter('f!windpress/core/runtime:is_prevent_load', fn (bool $is_prevent_load): bool => $this->is_prevent_load($is_prevent_load));
-        //     add_filter('f!windpress/core/runtime:append_header.exclude_admin', fn (bool $is_exclude_admin): bool => $this->is_exclude_admin($is_exclude_admin));
-        //     new Editor();
-        // }
+        if ($this->is_enabled()) {
+            add_filter('f!windpress/core/runtime:is_prevent_load', fn (bool $is_prevent_load): bool => $this->is_prevent_load($is_prevent_load));
+            add_filter('f!windpress/core/runtime:append_header.mission_control.is_prevent_load', fn (bool $is_prevent_load): bool => $this->is_prevent_load($is_prevent_load));
+            add_filter('f!windpress/core/runtime:append_header.exclude_admin', fn (bool $is_exclude_admin): bool => $this->is_exclude_admin($is_exclude_admin));
+            // new Editor();
+        }
     }
 
     public function get_name(): string
