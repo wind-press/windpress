173cf14d79607c2e2262d7ded0ecd124d859b305
Joshua Gugun Siagian <suabahasa@gmail.com>
2024-08-28 11:32:37 +0800

    Update VERSION for 3.0.2

--- deploy/deploy-scoped.sh
+++ deploy/deploy-scoped.sh
@@ -40,6 +40,7 @@ wget https://github.com/orgrosua/php-scoper-wordpress-excludes/archive/refs/head
 # extract whitelist of php-scoper
 note "Extracting whitelist of php-scoper"
 unzip "php-scoper-wordpress-excludes-master.zip" -d "$DEPLOY_DIRECTORY/deploy"
+rm -f "php-scoper-wordpress-excludes-master.zip"
 
 # move scoper.inc.php file from $DEPLOY_DIRECTORY to current directory
 # note "Moving scoper.inc.php file from $DEPLOY_DIRECTORY to current directory"
@@ -52,6 +53,7 @@ wget https://github.com/humbug/php-scoper/releases/download/0.18.14/php-scoper.p
 # Work around possible PHP memory limits
 note "Running scoper to $RESULT_DIRECTORY"
 php -d memory_limit=-1 php-scoper.phar add-prefix --output-dir "../$RESULT_DIRECTORY" --config "deploy/scoper.inc.php" --force --ansi --working-dir "$DEPLOY_DIRECTORY";
+rm -f "$RESULT_DIRECTORY/php-scoper.phar"
 
 # note "Dumping Composer Autoload"
 # composer dump-autoload --working-dir "$RESULT_DIRECTORY" --ansi --classmap-authoritative --no-dev
