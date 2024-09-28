65cd22d8e24c6d39102d8854e78862acf3c2d179
Joshua Gugun Siagian <suabahasa@gmail.com>
2024-09-28 09:59:24 +0700

    remove unused code

--- assets/packages/core/tailwindcss-v3/postcss-plugins/tailwindcss/index.js
+++ assets/packages/core/tailwindcss-v3/postcss-plugins/tailwindcss/index.js
@@ -2,8 +2,6 @@ import processTailwindFeatures from 'https://esm.sh/tailwindcss@3/src/processTai
 
 export default Object.assign(
     function (config, contents) {
-        console.log('config and contents', config, contents);
-
         return {
             postcssPlugin: 'tailwindcss',
             async Once(root, { result }) {
