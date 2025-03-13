d85e805f337c8e20464304e34b8dfa6e8f4862f4
Joshua Gugun Siagian <suabahasa@gmail.com>
2025-03-13 11:23:32 +0700

    wip

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from 'globals'
+import pluginVue from 'eslint-plugin-vue'
+import oxlint from 'eslint-plugin-oxlint';
+import tseslint from 'typescript-eslint';
+
+export default [
+    // add more generic rulesets here, such as:
+    // js.configs.recommended,
+    tseslint.configs.recommended,
+    ...pluginVue.configs['flat/recommended'],
+    {
+        rules: {
+            // override/add rules settings here, such as:
+            // 'vue/no-unused-vars': 'error'
+        },
+        languageOptions: {
+            sourceType: 'module',
+            globals: {
+                ...globals.browser
+            }
+        }
+    },
+    ...oxlint.configs['flat/recommended'], // oxlint should be the last one
+]
