2d0a19e9a530a32459fde0ba5e7810d86ccbf227
Joshua Gugun Siagian <suabahasa@gmail.com>
2025-03-19 02:19:15 +0700

    [TW4] fix play observer script

--- assets/packages/core/tailwindcss/play/observer.ts
+++ assets/packages/core/tailwindcss/play/observer.ts
@@ -1,121 +1,218 @@
-import { encodeBase64 } from '@std/encoding/base64';
-import { build } from '../build';
+import { compile as _compile } from '../build';
 import { decodeVFSContainer } from '../vfs';
+import { Instrumentation } from './instrumentation';
 
-let styleContainer: HTMLStyleElement | null = null;
+// Adapted from '@tailwindcss/browser' package
 
-let lastCandidateSet = new Set();
+// Warn users about using the browser build in production as early as possible.
+// It can take time for the script to do its work so this must be at the top.
+console.warn(
+    'The compiler of Tailwind CSS should not be used in production. To use Tailwind CSS in production, use the cached CSS: https://wind.press/docs/advanced/cache',
+)
 
-const vfsContainer = document.querySelector('script#windpress\\:vfs[type="text/plain"]');
+/**
+ * The list of all seen classes on the page so far. The compiler already has a
+ * cache of classes but this lets us only pass new classes to `build(…)`.
+ */
+let classes = new Set<string>()
 
-if (vfsContainer) {
-    initListener();
+/**
+ * The stylesheet that we use to inject the compiled CSS into the page.
+ */
+let sheet = document.createElement('style')
 
-    const vfsObserver = new MutationObserver(async () => {
-        await applyStyles();
-    });
+/**
+ * The queue of build tasks that need to be run. This is used to ensure that we
+ * don't run multiple builds concurrently.
+ */
+let buildQueue = Promise.resolve()
 
-    vfsObserver.observe(vfsContainer, {
-        characterData: true,
-        subtree: true
-    });
+/**
+ * What build this is
+ */
+let nextBuildId = 1
+
+/**
+ * Used for instrumenting the build process. This data shows up in the
+ * performance tab of the browser's devtools.
+ */
+let I = new Instrumentation()
+
+/**
+ * The last input VFS that was compiled. If script "change" without
+ * actually changing, we can avoid a full rebuild.
+ */
+let lastVFS = ''
+
+/**
+ * The current Tailwind CSS compiler.
+ *
+ * This gets recreated:
+ * - When VFS change
+ */
+let compiler: Awaited<ReturnType<typeof _compile>>;
+
+/**
+ * Create the Tailwind CSS compiler
+ *
+ * This handles loading imports, plugins, configs, etc…
+ *
+ * This does **not** imply that the CSS is actually built. That happens in the
+ * `build` function and is a separate scheduled task.
+ */
+async function createCompiler() {
+    I.start(`Create compiler`)
+    I.start('Reading VFS')
+
+    let script = document.querySelector('script#windpress\\:vfs[type="text/plain"]') as HTMLScriptElement | null;
+    if (!script) {
+        throw new Error('Script element with id "windpress:vfs" and type "text/plain" not found.');
+    }
+
+    let vfs = script?.textContent || '';
+
+    I.end('Reading VFS', {
+        size: vfs.length,
+        changed: lastVFS !== vfs,
+    })
+
+    // The input VFS did not change so the compiler does not need to be recreated
+    if (lastVFS === vfs) return
+
+    lastVFS = vfs
+
+    I.start('Compile CSS')
+    try {
+        compiler = await _compile({
+            // candidates: Array.from(candidates) as string[],
+            entrypoint: '/main.css',
+            volume: decodeVFSContainer(lastVFS || 'e30=')
+        })
+    } finally {
+        I.end('Compile CSS')
+        I.end(`Create compiler`)
+    }
+
+    classes.clear()
 }
 
-const domObserver = new MutationObserver(async (mutations) => {
-    const ignoredTags = ['STYLE', 'SCRIPT'];
-    let needsUpdate = true;
+async function build(kind: 'full' | 'incremental') {
+    if (!compiler) return
 
-    for (let mutation of mutations) {
-        const target = mutation.target as HTMLElement;
+    // 1. Refresh the known list of classes
+    let newClasses = new Set<string>()
 
-        if (target.nodeType === 1 && ignoredTags.includes((target as HTMLElement).tagName)) {
-            needsUpdate = false;
-        }
+    I.start(`Collect classes`)
 
-        for (let node of mutation.addedNodes) {
-            /**
-             * @type {HTMLElement}
-             */
-            const element = node;
+    for (let element of document.querySelectorAll('[class]')) {
+        for (let c of element.classList) {
+            if (classes.has(c)) continue
 
-            if (
-                element.nodeType === 1 &&
-                ignoredTags.includes((element as HTMLElement).tagName)
-            ) {
-                needsUpdate = false;
-            }
+            classes.add(c)
+            newClasses.add(c)
         }
     }
 
-    if (needsUpdate) {
-        await applyStyles();
-    }
-})
+    I.end(`Collect classes`, {
+        count: newClasses.size,
+    })
 
-domObserver.observe(document.documentElement, {
+    if (newClasses.size === 0 && kind === 'incremental') return
+
+    // 2. Compile the CSS
+    I.start(`Build utilities`)
+
+    sheet.textContent = compiler.build(Array.from(newClasses))
+
+    I.end(`Build utilities`)
+}
+
+
+
+function rebuild(kind: 'full' | 'incremental') {
+    async function run() {
+        if (!compiler && kind !== 'full') {
+            return
+        }
+
+        let buildId = nextBuildId++
+
+        I.start(`Build #${buildId} (${kind})`)
+
+        if (kind === 'full') {
+            await createCompiler()
+        }
+
+        I.start(`Build`)
+        await build(kind)
+        I.end(`Build`)
+
+        I.end(`Build #${buildId} (${kind})`)
+    }
+
+    buildQueue = buildQueue.then(run).catch((err) => I.error(err))
+}
+
+// // Handle changes to known stylesheets
+// let styleObserver = new MutationObserver(() => rebuild('full'))
+
+// function observeSheet(sheet: HTMLStyleElement) {
+//     styleObserver.observe(sheet, {
+//         attributes: true,
+//         attributeFilter: ['type'],
+//         characterData: true,
+//         subtree: true,
+//         childList: true,
+//     })
+// }
+
+// Handle changes to the document that could affect the styles
+// - Changes to any element's class attribute
+// - New stylesheets being added to the page
+// - New elements (with classes) being added to the page
+new MutationObserver((records) => {
+    let full = 0
+    let incremental = 0
+
+    for (let record of records) {
+        // // New stylesheets == tracking + full rebuild
+        // for (let node of record.addedNodes as Iterable<HTMLElement>) {
+        //     if (node.nodeType !== Node.ELEMENT_NODE) continue
+        //     if (node.tagName !== 'STYLE') continue
+        //     if (node.getAttribute('type') !== STYLE_TYPE) continue
+
+        //     observeSheet(node as HTMLStyleElement)
+        //     full++
+        // }
+
+        // New nodes require an incremental rebuild
+        for (let node of record.addedNodes) {
+            if (node.nodeType !== 1) continue
+
+            // Skip the output stylesheet itself to prevent loops
+            if (node === sheet) continue
+
+            incremental++
+        }
+
+        // Changes to class attributes require an incremental rebuild
+        if (record.type === 'attributes') {
+            incremental++
+        }
+    }
+
+    if (full > 0) {
+        return rebuild('full')
+    } else if (incremental > 0) {
+        return rebuild('incremental')
+    }
+}).observe(document.documentElement, {
     attributes: true,
     attributeFilter: ['class'],
+    childList: true,
     subtree: true,
-    childList: true
-});
+})
 
-async function applyStyles() {
-    const candidates = new Set();
+rebuild('full')
 
-    document.querySelectorAll('[class]').forEach((element) => {
-        element.classList.forEach((className) => candidates.add(className));
-    });
-
-    if (document.body && candidates.size > 0) {
-        if (!styleContainer || !styleContainer.isConnected) {
-            styleContainer = document.createElement('style');
-            document.head.append(styleContainer);
-        }
-
-        // compare the new candidates with the last set
-        if (lastCandidateSet.size === candidates.size) {
-            let isDifferent = false;
-
-            for (let candidate of candidates) {
-                if (!lastCandidateSet.has(candidate)) {
-                    isDifferent = true;
-                    break;
-                }
-            }
-
-            if (!isDifferent) {
-                return;
-            }
-        }
-
-        // update the last set
-        lastCandidateSet = candidates;
-
-        styleContainer.textContent = await build({
-            candidates: Array.from(candidates) as string[],
-            entrypoint: '/main.css',
-            volume: decodeVFSContainer(vfsContainer?.textContent || 'e30=')
-        });
-    }
-}
-
-// Ensure the styles are applied once (on load)
-await applyStyles();
-
-export function initListener() {
-    const channel = new BroadcastChannel('windpress');
-
-    channel.addEventListener('message', async (e) => {
-        const data = e.data;
-        const source = 'windpress/dashboard';
-        const target = 'windpress/observer';
-        const task = 'windpress.code-editor.saved';
-
-        if (data.source === source && data.target === target && data.task === task) {
-            if (vfsContainer) {
-                vfsContainer.textContent = encodeBase64(JSON.stringify(data.payload.volume));
-                await applyStyles();
-            }
-        }
-    })
-}
+document.head.append(sheet)
