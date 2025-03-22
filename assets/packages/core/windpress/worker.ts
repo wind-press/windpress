437aea7cf99133049c76e861b84341b69f005ca4
Joshua Gugun Siagian <suabahasa@gmail.com>
2025-03-23 00:21:49 +0700

    option for worker without lock

--- assets/packages/core/windpress/worker.ts
+++ assets/packages/core/windpress/worker.ts
@@ -1,54 +1,60 @@
 import { buildCache, type BuildCacheOptions } from './compiler';
 
-export async function setupWorker() {
+function cacheWorker() {
     const channel = new BroadcastChannel('windpress');
     let buildQueue: Promise<void> = Promise.resolve();
 
+    channel.addEventListener('message', async (e) => {
+        const data = e.data;
+        const source = ['windpress/dashboard', 'windpress/integration'];
+        const target = 'windpress/compiler';
+
+        if (source.includes(data.source) && data.target === target && data.task === 'generate-cache') {
+            buildQueue = buildQueue.then(async () => {
+                await buildCache(data.data as BuildCacheOptions).then(() => {
+                    channel.postMessage({
+                        source: 'windpress/compiler',
+                        target: 'windpress/dashboard',
+                        task: 'generate-cache.response',
+                        data: {
+                            status: 'success',
+                        }
+                    });
+                }).catch((e) => {
+                    channel.postMessage({
+                        source: 'windpress/compiler',
+                        target: 'windpress/dashboard',
+                        task: 'generate-cache.response',
+                        data: {
+                            status: 'error',
+                        }
+                    });
+
+                    console.error(e);
+                });
+
+            }).catch((e) => {
+                console.error(e);
+            });
+        }
+    });
+}
+
+export async function setupWorker() {
     if ('locks' in navigator) {
         navigator.locks.request("task_lock", { mode: "exclusive" }, lock => {
-            console.log("This worker is the leader.");
+            console.log("Lock acquired. Starting worker.");
 
-            channel.addEventListener('message', async (e) => {
-                const data = e.data;
-                const source = ['windpress/dashboard', 'windpress/integration'];
-                const target = 'windpress/compiler';
-
-                if (source.includes(data.source) && data.target === target && data.task === 'generate-cache') {
-                    buildQueue = buildQueue.then(async () => {
-                        await buildCache(data.data as BuildCacheOptions).then(() => {
-                            channel.postMessage({
-                                source: 'windpress/compiler',
-                                target: 'windpress/dashboard',
-                                task: 'generate-cache.response',
-                                data: {
-                                    status: 'success',
-                                }
-                            });
-                        }).catch((e) => {
-                            channel.postMessage({
-                                source: 'windpress/compiler',
-                                target: 'windpress/dashboard',
-                                task: 'generate-cache.response',
-                                data: {
-                                    status: 'error',
-                                }
-                            });
-
-                            console.error(e);
-                        });
-
-                    }).catch((e) => {
-                        console.error(e);
-                    });
-                }
-            });
+            // Start the cache worker
+            cacheWorker();
 
             // Keep the lock until the tab is closed
             return new Promise(() => { });
         }).catch(() => {
-            console.log("Another worker is the leader.");
+            console.log("Failed to acquire lock. Worker already running in another tab.");
         });
     } else {
-        console.log("Locks API not supported in this browser.");
+        console.log("Locks API not supported in this browser. Starting worker without lock.");
+        cacheWorker();
     }
 }
