696ff83ee539310a7a6f5f7bc00fb54c11ecbc9f
Joshua Gugun Siagian <suabahasa@gmail.com>
2025-03-28 02:13:11 +0700

    Update VERSION for 3.3.18

--- constant.php
+++ constant.php
@@ -26,7 +26,7 @@ class WIND_PRESS
     /**
      * @var string
      */
-    public const VERSION = '3.3.17';
+    public const VERSION = '3.3.18';
 
     /**
      * @var string
--- deploy/build-i18n.ts
+++ deploy/build-i18n.ts
@@ -7,14 +7,13 @@ const input = readFileSync('./languages/windpress.pot');
 
 let po = gettextParser.po.parse(input);
 
-let body;
+let body = '';
 
 for (const key in po.translations['']) {
-    if (key === '') {
-        continue;
+    if (key) {
+        // escape single quote
+        body += `\t__('${po.translations[''][key].msgid.replace(/'/g, "\\'")}', 'windpress');\n`;
     }
-    // escape single quote
-    body += `\t__('${po.translations[''][key].msgid.replace(/'/g, "\\'")}', 'windpress');\n`;
 }
 
 const content = `(() => {\n\tconst { __, _x, _n, sprintf } = wp.i18n;\n${body}\n});`;
