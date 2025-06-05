1d42db699c8d8ae39effa74ce25ae7744b9f789e
Joshua Gugun Siagian <suabahasa@gmail.com>
2025-06-05 19:46:22 +0700

    update block edit control

--- assets/integration/gutenberg/block-editor.jsx
+++ assets/integration/gutenberg/block-editor.jsx
@@ -7,7 +7,7 @@ import { createOnigurumaEngine } from 'shiki/engine/oniguruma';
 
 import { __ } from '@wordpress/i18n';
 import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, PanelRow, ButtonGroup, TextareaControl, Button } from '@wordpress/components';
+import { PanelBody, PanelRow, TextareaControl, Button } from '@wordpress/components';
 import autosize from 'autosize';
 import Tribute from 'tributejs';
 import { debounce } from 'lodash-es';
@@ -374,13 +374,14 @@ function addClassInspectorControls(BlockEdit) {
                 <InspectorControls>
                     <PanelBody title={__('WindPress', 'windpress')} icon={WindPressIcon} initialOpen={true}>
                         <PanelRow className='windpressgutenberg-actions'>
-                            <ButtonGroup >
-                                <Button showTooltip label={__('Automatic Class Sorting', 'windpress')} onClick={handleAutomaticClassSorting}>
+                            <div >
+                                <Button showTooltip variant='secondary' label={__('Automatic Class Sorting', 'windpress')} onClick={handleAutomaticClassSorting}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-reorder icon icon-tabler icons-tabler-outline icon-tabler-reorder"><path d="M3 15m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path><path d="M10 15m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path><path d="M17 15m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path><path d="M5 11v-3a3 3 0 0 1 3 -3h8a3 3 0 0 1 3 3v3"></path><path d="M16.5 8.5l2.5 2.5l2.5 -2.5"></path></svg>
                                 </Button>
-                            </ButtonGroup>
+                            </div>
                         </PanelRow>
                         <TextareaControl
+                            __nextHasNoMarginBottom
                             value={attributes.className}
                             onChange={(value) => handleValueChange(value)}
                             onInput={(e) => handleValueChange(e.target.value)}
