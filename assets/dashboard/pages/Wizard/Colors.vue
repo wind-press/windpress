2586aab5b432a7613c4894db653d86300dfb5fb4
Joshua Gugun Siagian <suabahasa@gmail.com>
2025-07-07 14:04:47 +0700

    cleanup

--- assets/dashboard/pages/Wizard/Colors.vue
+++ assets/dashboard/pages/Wizard/Colors.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import FluidCalculator from '@/dashboard/components/Wizard/FluidCalculator.vue';
 </script>
 <template>
     <div class="mx-auto my-auto">
-        <FluidCalculator />
-        <!-- <UAlert :title="i18n.__('Feature are coming soon', 'windpress')" :description="i18n.__('This section is under development.', 'windpress')" color="error" variant="soft" /> -->
+        <UAlert :title="i18n.__('Feature are coming soon', 'windpress')" :description="i18n.__('This section is under development.', 'windpress')" color="error" variant="soft" />
     </div>
 </template>
